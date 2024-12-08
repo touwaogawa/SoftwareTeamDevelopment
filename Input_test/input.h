800b9c545d6d26286d2067eb3d439ff0bde87661
avocado <ssp.10.01e@gmail.com>
2024-12-08 19:41:12 +0900

    input変更

--- Input_test/input.h
+++ Input_test/input.h
@@ -1,79 +1,32 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <string>
+// #include <joyconlib>
 
-enum class KeyCode {
-    SPACE,
-    ESCAPE
-};
-
-enum class JoyConButtonCode {
-    A,
-    B,
-    X,
-    y
-};
-
-enum class GamePadButtonCode {
-    A,
-    B,
-    X,
-    y
-};
-
-struct JoyStickState {
-    float x;
-    float y;
-};
-
-// シングルトンパターンクラス
 class Input {
 public:
-    static Input& GetInstance()
-    {
-        static Input instance; // ローカル静的変数でインスタンスを保持
-        return instance;
-    }
+    Input();
 
-    // 接続に成功したら trueを返す(joyconよりゲームパッドが優先される)
-    bool ConnectController();
+    // 接続に成功したら trueを返す
+    static bool ConnectController();
 
-    // メンバにキーの状態を保存
-    void ProcessInput();
+    // メンバにキーの状態を更新, 毎フレーム呼ばれる
+    static void ProcessInput();
 
     // PCキーボード関係(SDL)
-    bool GetKey(KeyCode keycode);
-    bool GetKeyDown(KeyCode keycode);
-    bool GetKeyUp(KeyCode keycode);
+    static bool GetKey(SDL_KeyCode keyCode);
+    static bool GetKeyDown(SDL_KeyCode keyCode);
+    static bool GetKeyUp(SDL_KeyCode keyCode);
 
-    // ジョイコン関係
-    bool GetJoyConButton(JoyConButtonCode jcbc);
-    bool GetJoyConButtonDown(JoyConButtonCode jcbc);
-    bool GetJoyConButtonUp(JoyConButtonCode jcbc);
-    JoyStickState GetJoyConStickState();
-    JoyStickState GetJoyConStickState(JoyStickState& joystickstate);
-    JoyStickState GetJoyConStickState(float& JoyStickState_x, float& JoyStickState_y);
-
-    // ゲームパッド関係
-    // bool GetGamePadButton(GamePadButtonCode jcbc);
-    // bool GetGamePadButtonDown(GamePadButtonCode jcbc);
-    // bool GetGamePadButtonUp(GamePadButtonCode jcbc);
-    // JoyStickState GetGamePadStickState();
-    // JoyStickState GetGamePadStickState(JoyStickState& joystickstate);
-    // JoyStickState GetGamePadStickState(float& JoyStickState_x, float& JoyStickState_y);
+    // ゲームパッドとジョイコン共通
+    static bool GetJoyConButton(std::string buttonName);
+    static bool GetJoyConButtonDown(std::string buttonName);
+    static bool GetJoyConButtonUp(std::string buttonName);
+    //-1~1の範囲で指定した名前の値が返される。
+    static float GetAxis(std::string axisName);
 
 private:
-    // コンストラクタをプライベートにすることで外部でインスタンス化できないように
-    Input()
-    {
-    }
-
-    // コピーコンストラクタと代入演算子を削除して、インスタンスの複製を防ぐ
-    Input(const Input&)            = delete;
-    Input& operator=(const Input&) = delete;
-
-    // ここに状態を格納するメンバを書く
-    // SDL(KeyUpやdownをサポートしてるので多分必要ない)
-    // JoyCon joyconlib_t のprevのみ必要かな多分
-    // GamePad 何が必要か分からん
+    // ジョイコンはひとつしかつながない前提なのでリストにしない
+    //  static joyconlib_t mJoyConLastState;
 };
