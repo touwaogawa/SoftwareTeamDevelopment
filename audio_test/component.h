269aff02a5f01d4e0ac237bcc6455c0764e83903
avocado <ssp.10.01e@gmail.com>
2024-12-17 12:34:54 +0900

    add audiotest

---
+++ audio_test/component.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class GameObject;
+
+class Component {
+public:
+    explicit Component(GameObject* owner)
+        : mOwner(owner)
+    {
+    }
+    ~Component()
+    {
+    }
+
+protected:
+    GameObject* mOwner;
+};
