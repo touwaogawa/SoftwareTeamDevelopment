285abbdea00c9337823b2ad0f26b1e35bdc61b62
avocado <ssp.10.01e@gmail.com>
2024-12-17 11:25:57 +0900

    動的ライブラリ
    プロジェクトディレクトリ内で動的ライブラリのリンクができたはず

--- client/src/input.h
+++ client/src/input.h
@@ -16,6 +16,7 @@ public:
 
     // メンバにキーの状態を更新, 毎フレーム呼ばれる
     static bool ProcessInput();
+    void SetInput(const joyconlib_t& joyCon, const Uint8& keyBordState);
 
     // PCキーボード関係(SDL)
     static bool GetKey(SDL_Scancode scancode);
@@ -30,8 +31,6 @@ public:
     static float GetAxis(std::string axisName);
 
 private:
-    // ジョイコンはひとつしかつながない前提なのでリストにしない
-    //  static joyconlib_t mJoyConLastState;
     static SDL_Event mEvent;
     static const Uint8* mKeyboardState;
     static Uint8 mPrevKeyboardState[SDL_NUM_SCANCODES];
