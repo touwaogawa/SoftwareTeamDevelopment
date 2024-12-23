837772b1355e808bde79a3abfcd31a9981591895
avocado <ssp.10.01e@gmail.com>
2024-12-23 15:33:37 +0900

    Create beySmashEngine.h

---
+++ client/src/beySmashEngine.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "commandData.h"
+#include "component.h"
+#include "components/behaviour.h"
+#include "components/meshRenderer.h"
+#include "components/transform.h"
+#include "gameObject.h"
+#include "input.h"
+#include "renderer.h"
+#include "scene.h"
+#include "time.h"
+#include <SDL2/SDL.h>
+#include <iostream>
