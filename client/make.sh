bdb3ee0861befa02fe3f723bc4d3ca38735ad0f1
avocado <ssp.10.01e@gmail.com>
2024-12-23 15:31:08 +0900

    変更

--- client/make.sh
+++ client/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # LD_LIBRARY_PATHに../libを追加
-export LD_LIBRARY_PATH=../lib
+export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
 
 # 環境変数が正しく設定されたか確認するために表示（オプション）
 echo "LD_LIBRARY_PATH is set to: $LD_LIBRARY_PATH"
